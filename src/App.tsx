f4dae7c7dfda8591ff6c9fba23c0513788063b33
aviad priel <aviad.priel@gmail.com>
2022-05-09 23:45:02 +0300

    qq

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import {Button, TextField} from "@mui/material";
+import {FormEventHandler, useState} from "react";
 
-function App() {
+
+
+export  function ButtonAppBar() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" >
+            מחשבון גימלים
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
 
-export default App;
+export default function App() {
+    const [text,setText] = useState("")
+    const handleSubmit:FormEventHandler<HTMLFormElement> = (e ) => {
+        e.preventDefault();
+        // @ts-ignore
+        window.aaa = e.target.date;
+        // @ts-ignore
+        let date = new Date(e.target.date.value);
+        const days_to_add = date.getHours() < 8 ? 1 : 2;
+        date.setDate(date.getDate() + days_to_add);
+        date.setHours(8);
+        date.setSeconds(0);
+        date.setMinutes(0);
+        const date_text = new Intl.DateTimeFormat('he-IL', { dateStyle: 'full', timeStyle: 'long' }).format(date);
+        setText(date_text)
+
+    }
+  return (<>
+  <ButtonAppBar/>
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+
+        <form onSubmit={handleSubmit}>
+           <TextField
+        id="date"
+        label="זמן קבלת הגימל"
+        type="datetime-local"
+        sx={{ width: 250 }}
+        InputLabelProps={{
+          shrink: true,
+        }}
+        name={"date"}
+        required
+      />
+          <Button type={"submit"} variant={"contained"} style={{marginRight:"10px"}}>חשב</Button>
+        </form>
+          <h1>{text}</h1>
+      </Box>
+    </Container>
+
+        </>
+
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import CssBaseline from '@mui/material/CssBaseline';
 import App from './App';
+import { ThemeProvider } from '@mui/material/styles';
 import reportWebVitals from './reportWebVitals';
+import theme from './theme';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+
+
+
 root.render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+    <CssBaseline />
     <App />
-  </React.StrictMode>
+  </ThemeProvider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
